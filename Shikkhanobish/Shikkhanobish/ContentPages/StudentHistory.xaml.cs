aa01836d14fb2cd317e9f2c6a8e63ab1d2391db1
Mahir <mahirmuzahid@gmail.com>
2019-12-17 01:47:09 +0600

    History added for both teacher and student

--- Shikkhanobish/Shikkhanobish/ContentPages/StudentHistory.xaml.cs
+++ Shikkhanobish/Shikkhanobish/ContentPages/StudentHistory.xaml.cs
@@ -1,7 +1,9 @@
-﻿using Syncfusion.XForms.Graphics;
+﻿using Newtonsoft.Json;
+using Syncfusion.XForms.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,11 +15,28 @@ namespace Shikkhanobish
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StudentHistory : ContentPage
     {
+        private List<TuitionHistoryStudent> studentHistory = new List<TuitionHistoryStudent>();
         public StudentHistory()
         {
             InitializeComponent();
-            BindingContext = new StudentHistoryViewModel();
-          
+            ShowTuitionSistoryAsync();
+
+           
+
+
+        }
+        public async void ShowTuitionSistoryAsync()
+        {
+            int StudentID = 1017;
+            string url = "https://api.shikkhanobish.com/api/Masters/GetTuitionHistoryStudent";
+            HttpClient client = new HttpClient();
+            string jsonData = JsonConvert.SerializeObject(new { StundentID = StudentID });
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(url, content).ConfigureAwait(true);
+            var result = await response.Content.ReadAsStringAsync().ConfigureAwait(true);
+            var hisotyList = JsonConvert.DeserializeObject<List<TuitionHistoryStudent>>(result);
+            studentHistory = hisotyList;
+            StudentHistoryListView.ItemsSource = studentHistory;
         }
     }
 }
